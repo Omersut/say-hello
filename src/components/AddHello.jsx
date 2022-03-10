74bd86312569000f084ea1dc4cef2f0483a80c38
Omer Sut <omersutcom@gmail.com>
2022-03-10 22:35:34 +0300

    go

--- src/components/AddHello.jsx
+++ src/components/AddHello.jsx
@@ -1,13 +1,25 @@
 import { useState,useEffect } from "react";
-
-import { addDoc, collection,serverTimestamp  } from "firebase/firestore";
-import db from "../db/Firebase";
+import { addDoc,serverTimestamp  } from "firebase/firestore";
+import { onSnapshot, query,orderBy } from "firebase/firestore";
 import { movieCollectionRef } from "../db/firebase-collection";
+import db from "../db/Firebase";
+
 
 function AddHello() {
   const [name, setname] = useState("");
   const [social, setsocial] = useState("");
+  const [hello, sethello] = useState([]);
 
+  useEffect(() => {
+    const q = query(movieCollectionRef,orderBy("timestamp", "desc"))
+    const unsubscribe = onSnapshot( q ,(snapshot) => {
+      sethello(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })));
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []); 
+  console.log(hello)
 
 
   
@@ -36,11 +48,14 @@ function AddHello() {
     <div className="container">
     <div className="input">
       <form onSubmit={handleSubmit} className="row input">
+      
         <div className="col-auto">
+          
           <label htmlFor="inputPassword2" className="visually-hidden">
             name
           </label>
           <input
+          style={{margin:"3px"}}
             value={name}
             onChange={(e) => setname(e.target.value)}
             type="text"
@@ -55,6 +70,7 @@ function AddHello() {
             social
           </label>
           <input
+          style={{margin:"3px"}}
             value={social}
             onChange={(e) => setsocial(e.target.value)}
             type="text"
@@ -64,15 +80,27 @@ function AddHello() {
           />
         </div>
 
-        {!JSON.parse(localStorage.getItem("hello")) && <div className="col-auto">
+        {!JSON.parse(localStorage.getItem("hello")) ? <div className="col-auto">
           <button
           
-            style={{ boxShadow: "5px 5px 5px rgba(1,0,0,0.5)" }}
+            style={{ boxShadow: "5px 5px 5px rgba(1,0,0,0.5)",margin:"5px" }}
             type="submit"
             className="btn btn-primary"
           >
             Send Hello
           </button>
+        </div>:<div className="col-auto">
+          
+          <label htmlFor="inputPassword2" className="visually-hidden">
+            name
+          </label>
+          <button
+          style={{ border: "solid 1px black",margin:"3px" }}
+          className="btn btn-warning"
+          type="button"
+        >
+          ▼ Nice to meet you <span className="badge bg-dark">{hello.length}</span>
+        </button>
         </div>}
       </form>
     </div></div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
+
 function Header() {
-    return ( <div style={{fontSize:"65px", marginTop:"49px"}}>
-        <strong>Say Hi!</strong>
-    </div> );
+
+  return (
+    <div>
+      <div style={{ justifyContent: "space-between",display:"flex" }}>
+      <div>
+       </div>
+        <div>
+          <img
+            style={{ width: "85px",margin:"5px" }}
+            src="https://firebase.google.com/downloads/brand-guidelines/SVG/logo-built_black.svg"
+          />
+        </div>
+      </div>
+
+     
+        <div style={{ fontSize: "65px", marginTop: "25px" }}>
+          <strong>Say Hi!</strong>
+        </div>
+   
+    </div>
+  );
 }
 
 export default Header;
